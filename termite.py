3d5e191c2fefa511c56e84370e02382f8acef378
Rubin Peci <rubinpeci@gmail.com>
2018-05-10 23:38:55 -0400

    Update font printing

--- termite.py
+++ termite.py
@@ -70,29 +70,30 @@ def theme_swap(t):
 
     return currentTheme
 
-#helper method to clean list of font files
-def clean(list):
-    for str in list:
-        if os.path.isfile(fonts_dir + str):
-            list.remove(str)
-
+#method to print out fonts
 def print_fonts():
-    #get list of font folders and clean the non-important ones
-    fonts = os.listdir(fonts_dir)
-    clean(fonts)
+    #gets the output of the command 'fc-list' which lists all the fonts installed
+    fonts = subprocess.run(['fc-list'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    #split it on each newline
+    fonts = fonts.split("\n")
 
-    #loop thru each folder
-    for font_folder in fonts:
-       #get the current directory it's working with and loop thru it
-       current_directory = os.listdir(fonts_dir + font_folder)
-       for font_file in current_directory:
-           #make sure it's a valid font file to be printed
-           if "." in font_file and not("fonts." in font_file) and not ("gz" in font_file):
-              if font_file[0] == ".": #if it's a hidden file, skip it
-                 break
-              else:                   #otherwise, print it without the file extension
-                  print(font_file[:font_file.index(".")])
-
+    #loop thru each font, and grabbing the important part (its name)
+    for font in fonts:
+        #makes sure its a valid entry in the list
+        if len(font) > 3:
+            #some fonts have commas and show up weird, so we append fix those here
+            if "," in font:
+                font = font[font.index(":") + 2:]
+                font = font[:font.index(",")]
+                #some of those fonts with commas also have colons so we fix that here
+                if ':' in font:
+                    font = font[:font.index(":")]   
+            #otherwise, we just grab the part in between the colons and print it out 
+            else:
+                font = font[font.index(":") + 2:]
+                font = font[:font.index(":")]   
+            print(font)
+ 
 def print_themes():
     #creates list of themes
     List = os.listdir(home + '/.config/themite/themes/termite/')
@@ -182,9 +183,7 @@ def main():
             for line in lines:
                 current.write(line)
 
-            
-        
-        
+#run main method  
 main()
 
 #exit from termite, and reopen
