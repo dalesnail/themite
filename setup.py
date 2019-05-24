47f6044b3d58dabe46fc33039889aabad84f3ece
dalesnail <cameronmills.work@gmail.com>
2019-05-23 23:08:59 -0400

    Added installer 'setup.py'

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import stat
+
+
+def make_executable(run):
+    mode = os.stat(run).st_mode
+    mode|= (mode & 0o444) >> 2
+    os.chmod(run, mode)
+
+mitepath = os.getcwd()
+run = "/usr/local/bin/themite"
+
+#Create file in runpath
+f = open(run,"w+")
+f.write(F"""#!/bin/sh
+python {mitepath}/themite.py""")
+f.close()
+
+#Make executable
+#os.chmod(run, stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+make_executable(run)
+
+print(mitepath)
